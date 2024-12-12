0b114bc36782b71849f1c76da8a267f5ff712657
karuuu0001 <karlosnino11@gmail.com>
2024-12-12 08:49:52 +0800

    1

--- backend/viewschedule.php
+++ backend/viewschedule.php
@@ -49,7 +49,8 @@ $result = $stmt->get_result();
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-        }
+        } 
+        
         /* General Styles */
         body {
             font-family: 'Arial', sans-serif;
